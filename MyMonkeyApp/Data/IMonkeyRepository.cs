d6dfb2d03c02e9553d30d336f6e06d1d4e67c1e8
Kerem1989 <98690898+Kerem1989@users.noreply.github.com>
2025-09-18 13:04:33 +0200

    Add IMonkeyRepository, InMemoryMonkeyRepository and unit test entry point (#3)

---
+++ MyMonkeyApp/Data/IMonkeyRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace MyMonkeyApp.Data;
+
+public interface IMonkeyRepository
+{
+    Task<IEnumerable<Monkey>> GetAllAsync();
+    Task<Monkey?> GetByNameAsync(string name);
+    Task<Monkey?> GetRandomAsync();
+}
